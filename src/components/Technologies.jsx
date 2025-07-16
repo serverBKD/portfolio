92cb2674d3bd723af4aeb1a03cd79b299a8791a2
serverBKD <carlosm1bravoa@gmail.com>
2025-07-16 18:56:43 -0400

    2025JUL16: v0.1.8: CSS Technologies Component + SVG

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,27 @@
+
+export default function Technologies() {
+  return (
+    <section className="techs">
+      <article>
+        <div>
+          <img src="/assets/techs/icons8-js.svg" alt="icons8-js.svg" />
+        </div>
+        <div>
+          <img src="/assets/techs/icons8-nodejs.svg" alt="icons8-js.svg" />
+        </div>
+        <div>
+          <img src="/assets/techs/icons8-react-native-96.png" alt="icons8-react-native-96.png" />
+        </div>
+        <div>
+          <img src="/assets/techs/icons8-nextjs.svg" alt="icons8-js.svg" />
+        </div>
+        <div>
+          <img src="/assets/techs/icons8-vuejs.svg" alt="icons8-vuejs.svg" />
+        </div>
+        <div>
+          <img src="/assets/techs/icons8-python.svg" alt="icons8-python.svg" />
+        </div>
+      </article>
+    </section>
+  )
+}
