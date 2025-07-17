8ee90eb82d4775a100852cd87429a4ce073d6137
serverBKD <carlosm1bravoa@gmail.com>
2025-07-17 11:13:11 -0400

    2025JUL17: v0.1.15: CallToAction

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,11 @@
+<template>
+	<section class="vue-section">
+		<div class="vue-div">
+			<h1 class="vue-h1">¿Tienes algún proyecto en mente?</h1>
+			<h2 class="vue-h2">¡Juntos hagamos realidad la web de tus sueños!</h2>
+			<a class="vue-link" href="#contacto">
+				contáctame
+			</a>
+		</div>
+	</section>
+</template>;
