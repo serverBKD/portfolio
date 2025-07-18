403e7ff0b0e29f2f7bc01c541f24eab82368905f
serverBKD <carlosm1bravoa@gmail.com>
2025-07-17 20:19:00 -0400

    2025JUL17: v0.1.22: CSS Linkbar

--- src/components/LinkBar.jsx
+++ src/components/LinkBar.jsx
@@ -6,21 +6,21 @@ export default function LinkBar() {
 		<section className='linkbar-box'>
 			<a
 				href='/'
-				className='linkbar-card text-x-link outline-2 outline-x-link hover:bg-x-link/30'
+				className='linkbar-card linkbar-items-1 '
 			>
 				<GlobeIcon />
 				<h3 className='text-xs'>SOURCE</h3>
 			</a>
 			<a
 				href='/'
-				className='linkbar-card text-x-success outline-2 outline-x-success hover:bg-x-success/30'
+				className='linkbar-card linkbar-items-2'
 			>
 				<GitHubIcon />
 				<h3 className='text-xs'>REPO</h3>
 			</a>
 			<a
 				href='/'
-				className='linkbar-card text-x-accent outline-2 outline-x-accent hover:bg-x-accent/30'
+				className='linkbar-card linkbar-items-3'
 			>
 				<YouTubeIcon />
 				<h3 className='text-xs'>VIDEO</h3>
