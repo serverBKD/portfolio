abc47ebe3541b37182b796bc0fbf55901db130ef
serverBKD <carlosm1bravoa@gmail.com>
2025-07-20 18:55:44 -0400

    2025JUL20: v0.1.26: CSS: btn WhatsApp

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -1,9 +1,9 @@
 <template>
-	<section class="vue-section">
-		<div class="vue-div">
-			<h1 class="vue-h1">¿Tienes algún proyecto en mente?</h1>
-			<h2 class="vue-h2">¡Juntos hagamos realidad la web de tus sueños!</h2>
-			<a class="vue-link" href="#contacto">
+	<section class="cta-section">
+		<div class="cta-div">
+			<h1 class="cta-h1">¿Tienes algún proyecto en mente?</h1>
+			<h2 class="cta-h2">¡Juntos hagamos realidad la web de tus sueños!</h2>
+			<a class="cta-link" href="#contacto">
 				contáctame
 			</a>
 		</div>
--- src/components/WSButton.vue
+++ src/components/WSButton.vue
@@ -1,10 +1,8 @@
 <template>
-  <section>
-    <article class="ws-btn">
+  <section class="ws-btn">
       <a href="https://api.whatsapp.com/send?phone=5491131234567&text=Hola%20cmba,%20me%20gustaría%20contactarte"
         target="_blank" rel="noopener noreferrer">
         <span></span>
       </a>
-    </article>
   </section>
 </template>
